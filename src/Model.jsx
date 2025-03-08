7ee76c06c0f588fd1b4c2feef84a01eaffe7cde3
nShirani <n.shirani37@gmail.com>
2025-03-08 02:15:52 -0800

    add fix position and scale

--- src/Model.jsx
+++ src/Model.jsx
@@ -24,6 +24,8 @@ export default function Model(props) {
           max: 2,
           step: 0.1,
         }, // Initial scale for object 4
+        "position fixed 4": false,
+        "scale fixed 4": false,
       }),
       Kabinet_2: folder({
         color5: { r: 255, g: 0, b: 0 },
@@ -33,6 +35,8 @@ export default function Model(props) {
           max: 3,
           step: 0.1,
         }, // Initial scale for object 5
+        "position fixed 5": false,
+        "scale fixed 5": false,
       }),
       Kabinet_3: folder({
         color6: { r: 0, g: 255, b: 0 },
@@ -42,6 +46,8 @@ export default function Model(props) {
           max: 1,
           step: 0.1,
         }, // Initial scale for object 6
+        "position fixed 6": false,
+        "scale fixed 6": false,
       }),
       Kabinet_4: folder({
         color7: { r: 0, g: 0, b: 255 },
@@ -51,6 +57,8 @@ export default function Model(props) {
           max: 4,
           step: 0.1,
         }, // Initial scale for object 7
+        "position fixed 7": false,
+        "scale fixed 7": false,
       }),
       Kabinet_5: folder({
         color8: { r: 128, g: 128, b: 128 },
@@ -60,6 +68,8 @@ export default function Model(props) {
           max: 2,
           step: 0.1,
         }, // Initial scale for object 8
+        "position fixed 8": false,
+        "scale fixed 8": false,
       }),
     }),
   });
@@ -76,6 +86,7 @@ export default function Model(props) {
 
     // Normalize scales to ensure they sum up to the target total scale
     let lastItemEnd = initialPositions[0]; // Track the end position of the last object
+
     objectIndices.forEach((index, i) => {
       const colorKey = `color${index}`; // e.g., "color4", "color5", etc.
       const scaleKey = `scaleX${index}`; // e.g., "scaleX4", "scaleX5", etc.
@@ -91,15 +102,84 @@ export default function Model(props) {
       nodes.Scene.children[index].material = material;
 
       // Update object scale
-      const normalizedScale =
-        (controls[scaleKey] / totalScale) * allKabinetScale;
+      // const normalizedScale =
+      //   (controls[scaleKey] / totalScale) * allKabinetScale;
 
-      // Update object position
-      const objectWidth = 3.119999885559082 * normalizedScale; // Assume object width is proportional to scale
-      nodes.Scene.children[index].position.x = lastItemEnd;
-      nodes.Scene.children[index].scale.x = normalizedScale;
-      lastItemEnd += objectWidth; // Update the end position for the next object
+      // // Update object position
+      // const objectWidth = 3.15 * normalizedScale; // Assume object width is proportional to scale
+      // nodes.Scene.children[index].position.x = lastItemEnd;
+      // nodes.Scene.children[index].scale.x = normalizedScale;
+      // lastItemEnd += objectWidth; // Update the end position for the next object
+
+      //
     });
+
+    // scales
+
+    const calculateScaleSums = (start, end) => {
+      let scaleSum = 0;
+      let fixScaleSum = 0;
+
+      for (let j = start; j < end; j++) {
+        if (controls[`scale fixed ${j}`]) {
+          fixScaleSum += nodes.Scene.children[j].scale.x;
+        } else {
+          scaleSum += controls[`scaleX${j}`];
+        }
+      }
+
+      return { scaleSum, fixScaleSum };
+    };
+
+    const processObjects = (start, end, scaleSum, fixScaleSum, maxScale) => {
+      for (let j = start; j < end; j++) {
+        if (controls[`scale fixed ${j}`]) {
+          const objectWidth = nodes.Scene.children[j].scale.x * 3.15;
+          nodes.Scene.children[j].position.x = lastItemEnd;
+          lastItemEnd += objectWidth;
+        } else {
+          const normalizedScale =
+            (controls[`scaleX${j}`] / scaleSum) * (maxScale - fixScaleSum);
+          const objectWidth = 3.15 * normalizedScale;
+
+          nodes.Scene.children[j].position.x = lastItemEnd;
+          nodes.Scene.children[j].scale.x = normalizedScale;
+          lastItemEnd += objectWidth;
+        }
+      }
+    };
+
+    let startIndex = objectIndices[0];
+    lastItemEnd = initialPositions[0];
+
+    for (
+      let i = startIndex;
+      i <= objectIndices[objectIndices.length - 1];
+      i++
+    ) {
+      if (controls[`position fixed ${i}`]) {
+        const maxScale =
+          (nodes.Scene.children[i].position.x - lastItemEnd) / 3.15;
+        const { scaleSum, fixScaleSum } = calculateScaleSums(startIndex, i);
+
+        processObjects(startIndex, i, scaleSum, fixScaleSum, maxScale);
+        startIndex = i;
+      }
+    }
+    const endPoint = initialPositions[0] + allKabinetScale * 3.15;
+    const maxScale = (endPoint - lastItemEnd) / 3.15;
+    const { scaleSum, fixScaleSum } = calculateScaleSums(
+      startIndex,
+      objectIndices[objectIndices.length - 1] + 1
+    );
+
+    processObjects(
+      startIndex,
+      objectIndices[objectIndices.length - 1] + 1,
+      scaleSum,
+      fixScaleSum,
+      maxScale
+    );
   }, [controls, nodes, allKabinetScale]);
 
   return <primitive object={scene} {...props} />;
