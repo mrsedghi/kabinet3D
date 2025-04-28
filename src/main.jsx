25d545a9d6647073caa0f7fd9d935cd92f0f4e94
mrsedghi <m.r.sedghii@gmail.com>
2025-04-28 16:46:40 +0330

    remove stats

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { Suspense } from "react";
-import { Stats } from "@react-three/drei";
 import { Leva } from "leva";
 import "./styles.css";
 import App from "./App";
@@ -8,7 +7,6 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Suspense fallback={null}>
     <App />
-    <Stats />
     <Leva oneLineLabels />
   </Suspense>
 );
